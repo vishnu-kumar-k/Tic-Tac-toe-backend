2d7fbb4b20b1f09d033aaad73c6eab2442a04933
vishnu-kumar-k <vishnuk55265@gmail.com>
2023-06-06 17:47:23 +0530

    l2

--- index.js
+++ index.js
@@ -1,37 +1,22 @@
-const express = require('express');
-const http = require('http');
-const socketIO = require('socket.io');
+const express = require("express");
+const http = require("http");
+const socketIO = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIO(server);
-
-// Serve static files from the public directory
-app.use(express.static('public'));
-
-// Set up a route for the homepage
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+const io = socketIO(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+var playersOnline=[]
+io.on("connection", (socket) => {
+  console.log("A user connected");
+    playersOnline.push(socket.id)
+    
 });
 
-// Socket.IO connection event
-io.on('connection', (socket) => {
-  console.log('A user connected');
-
-  // Handle custom events
-  socket.on('chat message', (msg) => {
-    console.log('Message received: ' + msg);
-    // Broadcast the message to all connected clients
-    io.emit('chat message', msg);
-  });
-
-  socket.on('disconnect', () => {
-    console.log('A user disconnected');
-  });
-});
-
-// Start the server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 server.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
